254ee99eead6c9528e2b93b885ea24dad5ad6fa5
Elza <elza.koleszar@stud.uni-corvinus.hu>
2020-10-02 21:17:17 +0200

    LoadData

--- negyedik_tok4cz/negyedik_tok4cz/Form1.cs
+++ negyedik_tok4cz/negyedik_tok4cz/Form1.cs
@@ -12,9 +12,20 @@ namespace negyedik_tok4cz
 {
     public partial class Form1 : Form
     {
+        List<Flat> Flats;
+        RealEstateEntities context = new RealEstateEntities();
+
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+
         }
+
+        private void LoadData()
+        {
+            Flats = context.Flats.ToList();
+        }
+
     }
 }
