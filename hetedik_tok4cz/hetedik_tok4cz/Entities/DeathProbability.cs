6e9024fd901ad9d52c876477255e9c17794c4259
Elza <elza.koleszar@stud.uni-corvinus.hu>
2020-10-25 18:45:37 +0100

    Classes

---
+++ hetedik_tok4cz/hetedik_tok4cz/Entities/DeathProbability.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hetedik_tok4cz.Entities
+{
+    class DeathProbability
+    {
+        public Gender Gender { get; set; }
+        public int Age { get; set; }
+        public double DthProbability { get; set; }
+
+    }
+}
