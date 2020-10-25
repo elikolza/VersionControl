f5f05c78b1838346b104e4d9e0d3c83f02fc81ab
Elza <elza.koleszar@stud.uni-corvinus.hu>
2020-10-25 20:26:11 +0100

    Simulation

--- hetedik_tok4cz/hetedik_tok4cz/Form1.cs
+++ hetedik_tok4cz/hetedik_tok4cz/Form1.cs
@@ -27,9 +27,30 @@ namespace hetedik_tok4cz
             DeathProbabilities = GetDeathProbabilities(@"C:\Temp\halál.csv");
         }
 
-        Random rng = new Random(1234);
+        private void Szimulacio()
+        {
+            Random rng = new Random();
+            // Végigmegyünk a vizsgált éveken
+            for (int year = 2005; year <= 2024; year++)
+            {
+                // Végigmegyünk az összes személyen
+                for (int i = 0; i < Population.Count; i++)
+                {
+                    // Ide jön a szimulációs lépés
+                }
 
-        public List<Person> GetPopulation(string csvpath)
+                int nbrOfMales = (from x in Population
+                                  where x.Gender == Gender.Male && x.IsAlive
+                                  select x).Count();
+                int nbrOfFemales = (from x in Population
+                                    where x.Gender == Gender.Female && x.IsAlive
+                                    select x).Count();
+                Console.WriteLine(
+                    string.Format("Év:{0} Fiúk:{1} Lányok:{2}", year, nbrOfMales, nbrOfFemales));
+            }
+        }
+
+            public List<Person> GetPopulation(string csvpath)
         {
             List<Person> population = new List<Person>();
             using (StreamReader sr = new StreamReader(csvpath, Encoding.Default))
