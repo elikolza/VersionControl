7582bc80196497d06d176749ca4a833175fdba81
Elza <elza.koleszar@stud.uni-corvinus.hu>
2020-11-12 19:32:36 +0100

    Ball

--- nyolcadik_tok4cz/nyolcadik_tok4cz/Entities/Ball.cs
+++ nyolcadik_tok4cz/nyolcadik_tok4cz/Entities/Ball.cs
@@ -1,33 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace nyolcadik_tok4cz.Entitites
+namespace nyolcadik_tok4cz.Entities
 {
-    public class Ball : Label
+    public class Ball: Label
     {
-        AutoSize = false;
-        Width = 50;
-        Height = Width;
-        Paint += Ball_Paint;
-    }
+        public Ball()
+        {
+            AutoSize = false;
+            Width = 50;
+            Height = Width;
+            Paint += Ball_Paint;
+        }
 
-    private void Ball_Paint(object sender, PaintEventArgs e)
-    {
-        DrawImage(e.Graphics);
-    }
+        private void Ball_Paint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics);
+        }
 
-    protected void DrawImage(Graphics g)
-    {
-        g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
-    }
+        protected void DrawImage(Graphics g)
+        {
+            g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
+        }
 
-    public void MoveBall()
-    {
-        Left += 1;
+        public void MoveBall()
+        {
+            Left += 1;
+        }
     }
-
 }
