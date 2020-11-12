2948390111d3bfd9567484bf0b609d8ed4269f96
Elza <elza.koleszar@stud.uni-corvinus.hu>
2020-11-12 20:03:59 +0100

    Conveyor_belt

--- nyolcadik_tok4cz/nyolcadik_tok4cz/Entities/BallFactory.cs
+++ nyolcadik_tok4cz/nyolcadik_tok4cz/Entities/BallFactory.cs
@@ -1,4 +1,4 @@
-﻿using nyolcadik_tok4cz.Entitites;
+﻿using nyolcadik_tok4cz.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace nyolcadik_tok4cz.Entities
 {
-    class BallFactory
+    public class BallFactory
     {
         public Ball CreateNew()
         {
