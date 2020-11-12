aeec02b37c91039d8a9905a446a06c965c4df0ae
Elza <elza.koleszar@stud.uni-corvinus.hu>
2020-11-12 19:29:48 +0100

    Ballfactory

--- nyolcadik_tok4cz/nyolcadik_tok4cz/Entities/BallFactory.cs
+++ nyolcadik_tok4cz/nyolcadik_tok4cz/Entities/BallFactory.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using nyolcadik_tok4cz.Entitites;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,9 @@ namespace nyolcadik_tok4cz.Entities
 {
     class BallFactory
     {
+        public Ball CreateNew()
+        {
+            return new Ball();
+        }
     }
 }
